92947c00201b0068442566023eff1610109d4492
isakbosman <isakbosman@users.noreply.github.com>
2020-06-04 13:11:25 -0700

    More changes to setup script

--- setup/gcp-install.sh
+++ setup/gcp-install.sh
@@ -8,7 +8,8 @@ sudo apt-get --assume-yes install software-properties-common
 
 # download and install GPU drivers
 wget "http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.2.89-1_amd64.deb" -O "cuda-repo-ubuntu1804_10.2.89-1_amd64.deb"
-
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+sudo apt-get update
 sudo dpkg -i cuda-repo-ubuntu1804_10.2.89-1_amd64.deb
 sudo apt-get update
 sudo apt-get -y install cuda
@@ -18,7 +19,6 @@ nvidia-smi
 # install Anaconda for current user
 mkdir downloads
 cd downloads
-sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 wget "https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh" -O "Anaconda3-2020.02-Linux-x86_64.sh"
 bash "Anaconda3-2020.02-Linux-x86_64.sh" -b
 
@@ -29,6 +29,7 @@ conda upgrade -y --all
 conda install pytorch torchvision cudatoolkit -y
 
 
+source ~/.bashrc
 jupyter notebook --generate-config
 jupass=`python -c "from notebook.auth import passwd; print(passwd())"`
 echo "c.NotebookApp.password = u'"$jupass"'" >> $HOME/.jupyter/jupyter_notebook_config.py
